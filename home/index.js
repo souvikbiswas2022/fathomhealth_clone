c8729b9f45e0e949c3a4b905e2d341812b86171e
gouravlala <gouravlala001@gmai.com>
2022-07-23 16:02:47 +0530

    alldone

---
+++ home/index.js
@@ -0,0 +1,6 @@
+
+import navbar from "./component/nav.js";
+import footer from "./component/footer.js"
+
+document.getElementById('footer').innerHTML= footer();
+document.getElementById('navbar').innerHTML = navbar();
