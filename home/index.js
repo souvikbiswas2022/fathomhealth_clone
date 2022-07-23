d0b3ae3f785be1890c8b1ba14dbf0270102c514d
Souvik Biswas <101393689+souvikbiswas2022@users.noreply.github.com>
2022-07-23 17:49:58 +0530

    Delete index.js

--- home/index.js
+++
@@ -1,6 +0,0 @@
-
-import navbar from "./component/nav.js";
-import footer from "./component/footer.js"
-
-document.getElementById('footer').innerHTML= footer();
-document.getElementById('navbar').innerHTML = navbar();
